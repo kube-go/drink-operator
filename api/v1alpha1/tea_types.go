00208907b22a283cbadd67f68627b789efd77f28
Jayapriya Pai <slashpai9@gmail.com>
2021-07-06 13:09:30 +0530

    update tea type

--- api/v1alpha1/tea_types.go
+++ api/v1alpha1/tea_types.go
@@ -23,6 +23,12 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+const (
+	PhasePending = "PENDING"
+	PhaseRunning = "RUNNING"
+	PhaseDone    = "DONE"
+)
+
 // TeaSpec defines the desired state of Tea
 type TeaSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
@@ -45,6 +51,9 @@ type TeaSpec struct {
 type TeaStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// Phase represents the state of the schedule: until the recipe is
+	// executed it is PENDING, afterwards it is DONE.
+	Phase string `json:"phase,omitempty"`
 }
 
 //+kubebuilder:object:root=true
